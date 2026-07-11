68d6e04f7439af4ba697310bba832d54be45df13
atharvakap <atharvakapile1156@gmail.com>
2026-07-12 02:44:51 +0530

    Navigation bar and glass theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        99999: '99999',
+      },
+    },
   },
   plugins: [],
 }
